bf1d0195242015ffa76671b603afaeeefecbb5d6
oscar <oscar@LAPTOP-G1N9MG0D>
2024-04-01 19:25:44 -0700

    creation of controllers, views, and change of nav bar

---
+++ COMP003B.Assignment2/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET "Info/Index"
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET "Info/AboutMe"
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+    }
+}
